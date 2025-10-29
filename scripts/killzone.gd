f67a13026634cc02660074c2e6a9502a32b06f12
Mercunus <parrot_2000@mail.ru>
2025-10-29 21:47:47 +0300

    Dying 2.0

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -4,10 +4,12 @@ extends Area2D
 @onready var timer: Timer = $Timer
 
 
-func _on_body_entered(_body):
+func _on_body_entered(body):
 	print("You died!") 
+	Engine.time_scale = 0.5
 	timer.start()
-
+	body.get_node("CollisionShape2D").queue_free()
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
